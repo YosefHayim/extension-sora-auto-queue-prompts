ec961e06a9281d64313a17075865085af403f165
YosefHayim <yosefisabag+03@gmail.com>
2025-09-21 06:07:25 +0300

    .

--- app/route.ts
+++ app/route.ts
@@ -1,3 +1,8 @@
+import { Ebay } from "@/lib/ebay/ebayClass";
+
 export async function GET(_request: Request) {
+  const ebay = new Ebay();
+  const instance = await ebay.auth.instance();
+  console.log(instance);
   return new Response("homepage page ssr");
 }

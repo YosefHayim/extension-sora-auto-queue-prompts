06a6b5a44f3c55b9e3ae0551881fe48438279939
YosefHayim <yosefisabag+03@gmail.com>
2025-09-21 08:56:37 +0300

    .

--- app/route.ts
+++ app/route.ts
@@ -1,9 +1,3 @@
-import { Ebay } from "@/lib/ebay/ebayClass";
-
 export async function GET(_request: Request) {
-  const ebay = new Ebay();
-  const instance = await ebay.auth.instance();
-  console.log(instance);
-  // biome-ignore lint/suspicious/useStaticResponseMethods: <explanation>
-  return new Response(JSON.stringify({ instance }));
+  return new Response("homepage ssr");
 }

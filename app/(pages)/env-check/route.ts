daca0d1954c75a7ec5e53c3bac247ff7a1970dec
YosefHayim <yosefisabag+03@gmail.com>
2025-09-21 22:44:00 +0300

    .

---
+++ app/(pages)/env-check/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(_request:NextRequest){
+  return new NextResponse({
+    appId:process.env.APP_ID_PROD,
+    devId:process.env.DEV_ID_PROD,
+    certId:process.env.CERT_ID_PROD
+  })
+}

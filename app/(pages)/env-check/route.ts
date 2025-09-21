5e871f8318505850266306a42f93736464df7a01
YosefHayim <yosefisabag+03@gmail.com>
2025-09-22 01:46:03 +0300

    .

--- app/(pages)/env-check/route.ts
+++ app/(pages)/env-check/route.ts
@@ -1,11 +1,15 @@
+import { getEnv } from "@daniel-rose/envex/server";
 import { type NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export async function GET(_request: NextRequest) {
+  const allEnv = getEnv();
+
   return new NextResponse({
-    appId: process.env.APP_ID_PROD,
-    devId: process.env.DEV_ID_PROD,
-    certId: process.env.CERT_ID_PROD,
+    APP_ID_PROD: allEnv.APP_ID_PROD || "",
+    DEV_ID_PROD: allEnv.DEV_ID_PROD || "",
+    CERT_ID_PROD: allEnv.CERT_ID_PROD || "",
+    DOPPLER_CONFIG: allEnv.DOPPLER_CONFIG || "",
+    DOPPLER_ENVIRONMENT: allEnv.DOPPLER_ENVIRONMENT || "",
+    REDIRECT_URI_PROD: allEnv.REDIRECT_URI_PROD || "",
   });
 }

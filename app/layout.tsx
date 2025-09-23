e2e8e8af5015010db2562921d15d59c4a38df73e
YosefHayim <yosefisabag+03@gmail.com>
2025-09-23 19:06:28 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/custom-global-components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* <Navbar /> */}
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
